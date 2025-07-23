f38919656f176a61ec087155c054663cd1d5632c
Ratludu <119836270+Ratludu@users.noreply.github.com>
2025-07-23 21:18:06 +1000

    completed multipart uploads

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
@@ -28,10 +29,52 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
-	// TODO: implement the upload here
+	// Parse form data
+	const maxMemory = 10 << 20
+	r.ParseMultipartForm(maxMemory)
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	// Get the image data from the form
+	file, header, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to parse form file", err)
+		return
+	}
+
+	defer file.Close()
+
+	contentType := header.Header.Get("Content-Type")
+	if contentType == "" {
+		respondWithError(w, http.StatusBadRequest, "Missing Content-Type", nil)
+	}
+
+	data, err := io.ReadAll(file)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Could not parse file", err)
+		return
+	}
+
+	vid, err := cfg.db.GetVideo(videoID)
+	if vid.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "Unauthorized", err)
+	}
+
+	thumb := thumbnail{
+		mediaType: contentType,
+		data:      data,
+	}
+
+	videoThumbnails[videoID] = thumb
+
+	newUrl := fmt.Sprintf("http://localhost:%s/api/thumbnails/%s", cfg.port, videoIDString)
+	vid.ThumbnailURL = &newUrl
+
+	err = cfg.db.UpdateVideo(vid)
+	if err != nil {
+		delete(videoThumbnails, videoID)
+		respondWithError(w, http.StatusInternalServerError, "Couldnt update video", err)
+	}
+
+	respondWithJSON(w, http.StatusOK, vid)
 }
